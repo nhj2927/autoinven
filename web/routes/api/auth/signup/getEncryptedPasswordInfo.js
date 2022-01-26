b4b6b6c889a6aabecc8f447a5d742da5dbed2654
hyunjun <nhj2927@naver.com>
2022-01-26 13:41:54 +0900

    chore: 에러처리문 추가

--- web/routes/api/auth/signup/getEncryptedPasswordInfo.js
+++ web/routes/api/auth/signup/getEncryptedPasswordInfo.js
@@ -3,23 +3,30 @@ module.exports = (password, salt) => {
 
   return new Promise((resolve, reject) => {
     crypto.randomBytes(64, (err, buf) => {
-      crypto.pbkdf2(
-        password,
-        salt || buf.toString('base64'),
-        100000,
-        64,
-        'sha512',
-        (err, key) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({
-              password: key.toString('base64'),
-              salt: buf.toString('base64'),
-            });
+      if (err) {
+        reject(err);
+      }
+      try {
+        crypto.pbkdf2(
+          password,
+          salt || buf.toString('base64'),
+          100000,
+          64,
+          'sha512',
+          (err, key) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve({
+                password: key.toString('base64'),
+                salt: buf.toString('base64'),
+              });
+            }
           }
-        }
-      );
+        );
+      } catch (err) {
+        resolve(err);
+      }
     });
   });
 };
