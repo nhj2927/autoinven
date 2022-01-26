7cf57a87b7d49f01b1eb48c21c12bec1b4915ada
hyunjun <nhj2927@naver.com>
2022-01-26 16:45:41 +0900

    feature: 내정보 수정 API 추가

--- web/routes/api/myinfo/index.js
+++ web/routes/api/myinfo/index.js
@@ -2,7 +2,22 @@ module.exports = (db) => {
   const express = require('express');
   const router = express.Router();
 
-  router.put('/', (req, res) => {});
+  const { doAsync } = require('$base/utils/asyncWrapper');
+  const editMyinfo = require('./editMyinfo');
+
+  //내정보 수정
+  router.put(
+    '/',
+    doAsync(async (req, res) => {
+      const { body: user_info } = req;
+
+      await editMyinfo(user_info, db);
+
+      res.status(200).json({
+        message: 'success',
+      });
+    })
+  );
 
   router.put('/password', (req, res) => {});
 
