6b0efebb0c91a5289c8c5e426750ec3cd12b1357
hyunjun <nhj2927@naver.com>
2022-02-07 14:31:23 +0900

    feat: 유저 창고 목록 변수 전달

--- web/routes/warehouse/index.js
+++ web/routes/warehouse/index.js
@@ -2,9 +2,22 @@ module.exports = (db) => {
   const express = require('express');
   const router = express.Router();
 
-  router.get('/', (req, res) => {
-    res.render('common/warehouse', {});
-  });
+  const { doAsync } = require('$base/utils/asyncWrapper');
+  const getUserWarehouses = require('./getUserWarehouses');
+
+  router.get(
+    '/',
+    doAsync(async (req, res) => {
+      const locale = res.locale;
+      const {
+        session: { email },
+      } = req;
+
+      const warehouses = await getUserWarehouses(db, email, locale);
+
+      res.render('common/warehouse', { warehouses });
+    })
+  );
 
   router.get('/:warehouse_id', (req, res) => {
     res.render('common/warehouseDetailWithItem', {});
