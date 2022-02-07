4413e5a61409731c6a3936dd4da67284219defeb
hyunjun <nhj2927@naver.com>
2022-02-07 15:49:09 +0900

    feat: 관리자 창고 관리 페이지 변수 전달

--- web/routes/admin/warehouse/index.js
+++ web/routes/admin/warehouse/index.js
@@ -1,6 +1,21 @@
+const { doAsync } = require('$base/utils/asyncWrapper');
+
 module.exports = (db) => {
   const express = require('express');
   const router = express.Router();
+
+  const getWarehouses = require('./getWarehouses');
+
+  router.get(
+    '/',
+    doAsync(async (req, res) => {
+      const locale = res.locale;
+
+      const warehouses = await getWarehouses(db, locale);
+
+      res.render('admin/adminWarehouse', { warehouses });
+    })
+  );
 
   router.get('/enroll', (req, res) => {
     res.render('admin/enrollWarehouse', {});
