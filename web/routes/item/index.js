005074b548771fe640e992e4f43a19171dd4446e
kjaehyeon <jacob9404@gmail.com>
2022-02-09 16:12:53 +0900

    qrPopup fin

--- web/routes/item/index.js
+++ web/routes/item/index.js
@@ -7,12 +7,11 @@ module.exports = (db) => {
   router.get(
     '/:item_id/qr',
     doAsync(async (req, res) => {
-      // const { item_id } = req.params;
-      // const item = await db.Item.findOne({ where: { item_id } });
-      // console.log(item.qrcode);
-      // const qr_code = await qrcode.toDataURL(item.qrcode);
-      // res.render('partial/qrPopup', { qr_code });
-      res.render('partial/qrPopup', { qr_code: '/image/test_image_1.jpg' });
+      const { item_id } = req.params;
+      const item = await db.Item.findOne({ where: { item_id } });
+      console.log(item.qrcode);
+      const qr_code = await qrcode.toDataURL(item.qrcode);
+      res.render('partial/qrPopup', { qr_code });
     })
   );
 
