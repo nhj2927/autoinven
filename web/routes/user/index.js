a4785d0bdd046c9f7517c2eb3b91582a7f45744e
hyunjun <nhj2927@naver.com>
2022-02-09 15:36:10 +0900

    chore: 파라미터 검사 조건 추가
    파라미터 모두 있는 경우 통과 -> 창고ID를 제외하고 모두 없는 경우도 통과

--- web/routes/user/index.js
+++ web/routes/user/index.js
@@ -16,12 +16,36 @@ module.exports = (db) => {
         query: { start_date, end_date, selected_area, available_area },
       } = req;
 
+      // 창고ID를 제외한 파라미터가 모두 있거나 없는 경우만 통과
+      const checkParameter = (
+        warehouse_id,
+        start_date,
+        end_date,
+        selected_area,
+        available_area
+      ) => {
+        return (
+          (warehouse_id &&
+            !start_date &&
+            !end_date &&
+            !selected_area &&
+            !available_area) ||
+          (warehouse_id &&
+            start_date &&
+            end_date &&
+            selected_area &&
+            available_area)
+        );
+      };
+
       if (
-        !warehouse_id ||
-        !start_date ||
-        !end_date ||
-        !selected_area ||
-        !available_area
+        !checkParameter(
+          warehouse_id,
+          start_date,
+          end_date,
+          selected_area,
+          available_area
+        )
       ) {
         const error = new Error('Insufficient parameter');
         error.statusCode = 400;
