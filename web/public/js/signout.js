b8c1ab3fd4a1f5efbf7b586b8b897afb9c1c577f
Ktaewon <dydalehdxo@naver.com>
2022-02-11 15:50:36 +0900

    logout fixed

--- web/public/js/signout.js
+++ web/public/js/signout.js
@@ -1,31 +1,29 @@
-(function ($) {
-  'use strict';
-  $(document).ready(function () {
-    var swalError = (text, callback) =>
-      Swal.fire({
-        icon: 'error',
-        title: 'Fail',
-        text: text,
-      }).then(callback);
-    $('#signout').on('click', function () {
-      console.log('clicked');
-      $.ajax({
-        url: '/api/auth/signout',
-        type: 'GET',
-        datatype: 'application/json',
-        success: (data) => {
-          if (data.message == 'success') {
-            window.location.href = '/';
-          }
-        },
-        error: function (request, status, error) {
-          Swal.fire({
-            title: 'Error',
-            html: `code: ${request.status}<br>message: 서버 내부 오류가 발생하였습니다. 잠시후 다시 시도해주세요.<br>error: ${error}`,
-            icon: 'error',
-          });
-        },
-      });
+$(document).ready(function () {
+  console.log('document ');
+  const swalError = (text, callback) =>
+    Swal.fire({
+      icon: 'error',
+      title: 'Fail',
+      text: text,
+    }).then(callback);
+  $('#signout').on('click', () => {
+    console.log('clicked');
+    $.ajax({
+      url: '/api/auth/signout',
+      type: 'GET',
+      datatype: 'application/json',
+      success: (data) => {
+        if (data.message == 'success') {
+          window.location.href = '/';
+        }
+      },
+      error: function (request, status, error) {
+        Swal.fire({
+          title: 'Error',
+          html: `code: ${request.status}<br>message: 서버 내부 오류가 발생하였습니다. 잠시후 다시 시도해주세요.<br>error: ${error}`,
+          icon: 'error',
+        });
+      },
     });
   });
 });
