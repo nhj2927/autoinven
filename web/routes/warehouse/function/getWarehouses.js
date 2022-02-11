ed503e0ca1ac5f946777521c5c410094ca8b16e1
hyunjun <nhj2927@naver.com>
2022-02-11 17:23:13 +0900

    fix: 창고목록페이지 변수 role에 따라 분리

--- web/routes/warehouse/function/getWarehouses.js
+++ web/routes/warehouse/function/getWarehouses.js
@@ -1,19 +1,17 @@
-const getImages = (images) => {
+const { fn, col } = require('sequelize');
+const getFullAddress = require('$base/utils/getFullAddress');
+const getLocaleLanguageValue = require('$base/utils/getLocaleLanguageValue');
+
+const getImage = (images) => {
   if (!images || !images.length) {
-    return [];
+    return null;
   } else {
-    return images.map((img) => {
-      return img.url;
-    });
+    return images[0].url;
   }
 };
 
-module.exports = async (db, locale, user_email) => {
-  const { fn, col } = require('sequelize');
-  const getFullAddress = require('$base/utils/getFullAddress');
-  const getLocaleLanguageValue = require('$base/utils/getLocaleLanguageValue');
-  const where_clause = user_email ? { user_email, c_state_id: 3 } : null;
-
+// 유저와 계약된 창고목록
+const getMyWarehouses = async (db, locale, user_email) => {
   const contracts_result = await db.LeaseContract.findAll({
     attributes: [
       [
@@ -26,7 +24,7 @@ module.exports = async (db, locale, user_email) => {
       ],
       'lease_area',
     ],
-    where: where_clause,
+    where: { user_email, c_state_id: 3 },
     include: {
       model: db.Warehouse,
       required: true,
@@ -47,7 +45,7 @@ module.exports = async (db, locale, user_email) => {
     },
   });
 
-  const warehouses = contracts_result.map((contract) => {
+  return contracts_result.map((contract) => {
     return {
       warehouse_id: contract.Warehouse.warehouse_id,
       start_date: contract.start_date,
@@ -70,9 +68,61 @@ module.exports = async (db, locale, user_email) => {
       ),
       area: contract.lease_area,
       is_verified: contract.Warehouse.is_verified,
-      image_url: getImages(contract.Warehouse.WarehouseImages),
+      image: getImage(contract.Warehouse.WarehouseImages),
     };
   });
+};
+
+// 모든 창고목록
+const getAllWarehouses = async (db, locale) => {
+  const warehouses_result = await db.Wareshouse.findAll({
+    attributes: [
+      'warehouse_id',
+      'name_ko',
+      'name_en',
+      'address1_ko',
+      'address1_en',
+      'address2_ko',
+      'address2_en',
+      'is_verified',
+    ],
+    include: {
+      model: db.WarehouseImage,
+      attributes: ['url'],
+    },
+  });
+
+  return warehouses_result.map((warehouse) => {
+    return {
+      warehouse_id: warehouse.warehouse_id,
+      name: getLocaleLanguageValue(
+        locale,
+        warehouse.name_ko,
+        warehouse.name_en
+      ),
+      address: getLocaleLanguageValue(
+        locale,
+        getFullAddress(warehouse.address1_ko, warehouse.address2_ko),
+        getFullAddress(warehouse.address1_en, warehouse.address2_en)
+      ),
+      is_verified: contract.Warehouse.is_verified,
+      images: getImage(warehouse.WarehouseImages),
+    };
+  });
+};
+
+module.exports = async (db, locale, user_email) => {
+  let warehouses = [];
+
+  // 유저일 경우
+  if (user_email) {
+    warehouses = getMyWarehouses(db, locale, user_email);
+  }
+
+  // 관리자일 경우
+  else {
+    warehouses = getAllWarehouses(db, locale);
+  }
 
   return warehouses;
 };
