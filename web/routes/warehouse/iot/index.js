e1f22c2f91ea25eedd33f6a011c9906772da0683
hyunjun <nhj2927@naver.com>
2022-02-14 15:49:23 +0900

    feat: iot monitoring 변수 전달

--- web/routes/warehouse/iot/index.js
+++ web/routes/warehouse/iot/index.js
@@ -1,11 +1,26 @@
 module.exports = (db) => {
   const express = require('express');
-  const router = express.Router();
+  const router = express.Router({ mergeParams: true });
+
+  const { doAsync } = require('$base/utils/asyncWrapper');
+  const getWarehouseDetailForIot = require('./function/getWarehouseDetailForIot');
 
   // 창고 모니터링
-  router.get('/monitoring', (req, res) => {
-    res.render('iot/monitoring');
-  });
+  router.get(
+    '/monitoring',
+    doAsync(async (req, res) => {
+      const {
+        params: { id: warehouse_id },
+      } = req;
+      const {
+        session: { name },
+      } = req;
+
+      const warehouse = await getWarehouseDetailForIot(db, warehouse_id);
+
+      res.send(warehouse);
+    })
+  );
 
   // 창고 통계
   router.get('/statistics', (req, res) => {
